3951f2e7fc1c365b8dd3d05a0edf93bb6ceb7abf
Ahmet MEYDAN <asmeydan0816@gmail.com>
2022-12-11 13:15:10 +0300

    title added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Cover from '../components/Cover'
 import Information from '../components/Information'
 import Layout from '../components/Layout'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>asmeydan</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Cover />
       <Information />
     </Layout>
--- pages/skills.js
+++ pages/skills.js
@@ -11,22 +11,26 @@ import tailwind from "../images/tailwind.png";
 import nodejs from "../images/nodejs.png";
 import mongodb from "../images/mongodb.png";
 import redux from "../images/redux.png";
+import Head from "next/head";
 
 const skills = () => {
-  
   return (
     <Layout>
+      <Head>
+        <title>asmeydan -skills</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <div className=" p-20 bg-white grid gap-4 min-height-screen place-content-center place-items-center grid-cols-2 sm:grid-cols-4 md:grid-cols-8">
-        <Image src={html} width={50} alt="html"/>
-        <Image src={css} width={50} alt="html"/>
-        <Image src={js} width={50} alt="html"/>
-        <Image src={react} width={50} alt="html"/>
-        <Image src={nextjs} width={75} alt="html"/>
-        <Image src={redux} width={50} alt="html"/>
-        <Image src={bootstrap} width={50} alt="html"/>
-        <Image src={tailwind} width={50} alt="html"/>
-        <Image src={nodejs} width={50} alt="html"/>
-        <Image src={mongodb} width={50} alt="html"/>
+        <Image src={html} width={50} alt="html" />
+        <Image src={css} width={50} alt="html" />
+        <Image src={js} width={50} alt="html" />
+        <Image src={react} width={50} alt="html" />
+        <Image src={nextjs} width={75} alt="html" />
+        <Image src={redux} width={50} alt="html" />
+        <Image src={bootstrap} width={50} alt="html" />
+        <Image src={tailwind} width={50} alt="html" />
+        <Image src={nodejs} width={50} alt="html" />
+        <Image src={mongodb} width={50} alt="html" />
       </div>
     </Layout>
   );
